5978cce33439c019a092218e7488194f825cadc0
Johny Thomas <johnythomas.in@gmail.com>
2021-05-07 05:32:17 +0530

    runtime added

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import bs4.element
 import requests
 from bs4 import BeautifulSoup
-
+from mailer import send_mail
 
 
 # URL = "https://www.casioindiashop.com/Watches/AD249/Casio-Youth-Series-WS-1200H-3AVDF-(AD249)-Digital-Watch.html"
@@ -13,5 +13,9 @@ divTag = soup.find("div", attrs={"class": "price"}).find(attrs={"class": "flbuts
 print(divTag)
 if divTag.find("a", attrs={"class": "cart-buy"}) or divTag.find("a", "Buy Now"):
     print("Available")
+    send_mail("Your watch is now available", "<a href=\"https://www.casioindiashop.com/Watches/D218/Casio-Youth-Series"
+                                             "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
 else:
     print("Not Available")
+    send_mail("Out of stock", "<a href=\"https://www.casioindiashop.com/Watches/D218/Casio-Youth-Series"
+                                             "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
