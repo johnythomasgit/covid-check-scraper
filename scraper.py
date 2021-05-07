d93e9e4822dabee6c597f25726c48ac8a2dbf5c0
Johny Thomas <johnythomas.in@gmail.com>
2021-05-07 17:05:26 +0530

    updated covid check

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ import json
 
 
 def send_mail(subject, message):
+
     # create message object instance
     msg = email.message.Message()
 
@@ -18,14 +19,13 @@ def send_mail(subject, message):
     msg['From'] = "johnythomas.online@gmail.com"
     msg['To'] = "johnyvtk@gmail.com"
     msg['Subject'] = subject + " " + datetime.datetime.now(timezone('Asia/Kolkata')).strftime("%b,%d %I:%M %p")
-    print(msg['Subject'])
+
     # add in the message body
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(message)
 
     # create server
     server = smtplib.SMTP('smtp.gmail.com: 587')
-
     server.starttls()
 
     # Login Credentials for sending the mail
@@ -40,31 +40,30 @@ def send_mail(subject, message):
 
 
 def covid_center_search():
+
     available_centers = []
     for period in range(32):
         new_date = datetime.datetime.today() + datetime.timedelta(days=period)
         new_date_str = new_date.strftime("%d-%m-%Y")
         covin_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=304" \
                     "&date=" + new_date_str
-        print(covin_url)
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                    'Accept-Language': 'en-US,en;q=0.8',
                    'Host': 'cdn-api.co-vin.in',
                    'Connection': 'keep-alive'
                    }
-        print(headers)
         # req = urllib.request.Request(url=covin_url, headers=headers)
         # response = urllib.request.urlopen(req).read().decode('utf-8')
         response = requests.get(covin_url, headers=headers)
         print(response.status_code)
         print(response.json())
         response_json = response.json()
+
         for center in response_json.get("centers"):
             for session in center.get("sessions"):
                 if int(session.get("min_age_limit")) < 45:
                     available_centers.append(center.get("name") + " on " + new_date_str)
-        print(new_date_str + " " + str(len(available_centers)))
 
     print("total-" + str(len(available_centers)))
     if len(available_centers) > 0:
