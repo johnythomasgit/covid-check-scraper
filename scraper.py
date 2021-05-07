9f21d0a5165c155cd0e7b43ef58902c2104306bf
Johny Thomas <johnythomas.in@gmail.com>
2021-05-07 05:46:42 +0530

    updated

--- scraper.py
+++ scraper.py
@@ -1,7 +1,39 @@
 import bs4.element
 import requests
 from bs4 import BeautifulSoup
-from mailer import send_mail
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+
+def send_mail(subject, message):
+    # create message object instance
+    msg = MIMEMultipart()
+
+    # setup the parameters of the message
+    password = "jgmanjbbv"
+    msg['From'] = "johnythomas.online@gmail.com"
+    msg['To'] = "johnyvtk@gmail.com"
+    msg['Subject'] = subject
+
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+
+    # create server
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+
+    server.starttls()
+
+    # Login Credentials for sending the mail
+    server.login(msg['From'], password)
+
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+    print("mail send successfully")
+
 
 if __name__ == "__main__":
     # URL = "https://www.casioindiashop.com/Watches/AD249/Casio-Youth-Series-WS-1200H-3AVDF-(AD249)-Digital-Watch.html"
@@ -13,9 +45,10 @@ if __name__ == "__main__":
     print(divTag)
     if divTag.find("a", attrs={"class": "cart-buy"}) or divTag.find("a", "Buy Now"):
         print("Available")
-        send_mail("Your watch is now available", "<a href=\"https://www.casioindiashop.com/Watches/D218/Casio-Youth-Series"
-                                                 "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
+        send_mail("Your watch is now available",
+                  "<a href=\"https://www.casioindiashop.com/Watches/D218/Casio-Youth-Series"
+                  "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
     else:
         print("Not Available")
         send_mail("Out of stock", "<a href=\"https://www.casioindiashop.com/Watches/D218/Casio-Youth-Series"
-                                                 "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
+                                  "-AE-1500WH-1AVDF-(D218)-Digital-Watch.html\"> check it now</a>")
