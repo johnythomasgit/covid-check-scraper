928e22f5870dbb798c41b0487a3d78636e5cdcdc
Johny Thomas <johnythomas.in@gmail.com>
2021-05-14 00:37:57 +0530

    updated covid check

--- stock_checker.py
+++ stock_checker.py
@@ -11,14 +11,13 @@ import time
 
 
 def send_mail(subject, message):
-
     # create message object instance
     msg = email.message.Message()
-
+    mail_list = ["jyothisvthomas@gmail.com", "johnyvtk@gmail.com"]
     # setup the parameters of the message
     password = "jgmanjbbv"
     msg['From'] = "johnythomas.online@gmail.com"
-    msg['To'] = "johnyvtk@gmail.com"
+    msg['To'] = ", ".join(mail_list)
     msg['Subject'] = subject + " " + datetime.datetime.now(timezone('Asia/Kolkata')).strftime("%b,%d %I:%M %p")
 
     # add in the message body
@@ -33,7 +32,7 @@ def send_mail(subject, message):
     server.login(msg['From'], password)
 
     # send the message via the server.
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.sendmail(msg['From'], mail_list, msg.as_string())
 
     server.quit()
 
@@ -41,19 +40,19 @@ def send_mail(subject, message):
 
 
 def covid_center_search():
-
     available_centers = []
     for period in range(32):
         new_date = datetime.datetime.today() + datetime.timedelta(days=period)
         new_date_str = new_date.strftime("%d-%m-%Y")
         covin_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=304" \
                     "&date=" + new_date_str
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                   'Accept-Language': 'en-US,en;q=0.8',
-                   'Host': 'cdn-api.co-vin.in',
-                   'Connection': 'keep-alive'
-                   }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Host': 'cdn-api.co-vin.in',
+            'Connection': 'keep-alive'
+            }
         # req = urllib.request.Request(url=covin_url, headers=headers)
         # response = urllib.request.urlopen(req).read().decode('utf-8')
         response = requests.get(covin_url, headers=headers)
