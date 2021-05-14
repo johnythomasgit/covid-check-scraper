95c73fda8aa3758f5303faaaea486a3b8c54392d
Johny Thomas <johnythomas.in@gmail.com>
2021-05-14 15:34:55 +0530

    updated covid check

--- stock_checker.py
+++ stock_checker.py
@@ -1,4 +1,5 @@
 # web scraping functions for checking stock availability
+import sys
 import requests
 from bs4 import BeautifulSoup
 import email
@@ -7,46 +8,56 @@ import datetime
 from pytz import timezone
 import urllib
 import json
+import socket
+import os
+import time
 
 
 def send_mail(subject, message):
     # create message object instance
-    msg = email.message.Message()
-    #
-    mail_list = ["jyothisvthomas@gmail.com", "johnyvtk@gmail.com"]
-    # setup the parameters of the message
-    password = "jgmanjbbv"
-    msg['From'] = "johnythomas.online@gmail.com"
-    msg['To'] = ", ".join(mail_list)
-    msg['Subject'] = subject + " " + datetime.datetime.now(timezone('Asia/Kolkata')).strftime("%b,%d %I:%M %p")
+    global server
+    try:
+        msg = email.message.Message()
+        #
+        mail_list = ["jyothisvthomas@gmail.com", "johnyvtk@gmail.com"]
+        # setup the parameters of the message
+        password = "jgmanjbbv"
+        msg['From'] = "johnythomas.online@gmail.com"
+        msg['To'] = ", ".join(mail_list)
+        msg['Subject'] = subject + " " + datetime.datetime.now().strftime("%b,%d %I:%M %p")
 
-    # add in the message body
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(message)
+        # add in the message body
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(message)
 
-    # create server
-    server = smtplib.SMTP('smtp.gmail.com: 587')
-    server.starttls()
+        # create server
+        server = smtplib.SMTP('smtp.gmail.com: 587')
+        server.starttls()
 
-    # Login Credentials for sending the mail
-    server.login(msg['From'], password)
+        # Login Credentials for sending the mail
+        server.login(msg['From'], password)
 
-    # send the message via the server.
-    server.sendmail(msg['From'], mail_list, msg.as_string())
+        # send the message via the server.
+        server.sendmail(msg['From'], mail_list, msg.as_string())
 
-    server.quit()
+        server.quit()
 
-    print("mail send successfully")
+        print("mail send successfully")
+    except socket.error as e:
+        print(" Could not connect to mail server - ")
+    except:
+        print("Unknown error:" + sys.exc_info()[0])
 
 
 def covid_center_search():
+    print("covid center availability search - " + datetime.datetime.now().strftime("%H:%M:%S"))
     available_centers = []
     available_count = 0
     elder_available_centers = []
     elder_available_count = 0
     now = datetime.datetime.now()
-    today4_56 = now.replace(hour=4, minute=56, second=0, microsecond=0)
-    today5_04 = now.replace(hour=5, minute=4, second=0, microsecond=0)
+    today4_56 = now.replace(hour=16, minute=56, second=0, microsecond=0)
+    today5_04 = now.replace(hour=17, minute=4, second=0, microsecond=0)
 
     for period in range(32):
         new_date = datetime.datetime.today() + datetime.timedelta(days=period)
@@ -79,19 +90,22 @@ def covid_center_search():
                     elder_available_count += int(session['available_capacity'])
 
     if available_count > 0:
-        # print(available_centers)
+        print("available_count - " + available_count)
         send_mail("Covid Vaccine available for youth", ",<br/>".join(available_centers))
     else:
         print("Not available")
         # send_mail("No Luck ", "covid vaccine not available")
 
     if elder_available_count > 0:
+        print("elder_available_count - " + elder_available_count)
         send_mail("Covid Vaccine available for senior citizens", ",<br/>".join(elder_available_centers))
     if today4_56 < now < today5_04:
         if len(elder_available_centers) > 0:
+            print("elder_available_centers - " + str(len(elder_available_centers)))
             send_mail("Covid Vaccine status for senior citizens", ",<br/>".join(elder_available_centers))
         if len(available_centers) > 0:
-            send_mail("Covid Vaccine status for youth", ",<br/>".join(elder_available_centers))
+            print("available_centers - " + str(len(available_centers)))
+            send_mail("Covid Vaccine status for youth", ",<br/>".join(available_centers))
 
 
 def product_availability_search():
@@ -113,5 +127,7 @@ def product_availability_search():
 
 
 if __name__ == "__main__":
+    os.environ['TZ'] = 'Asia/Kolkata'
+    time.tzset()
     # product_availability_search()
     covid_center_search()
