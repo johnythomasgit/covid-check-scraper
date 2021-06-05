2d69ff0d0e609387dda955e428b5d910029f6a78
Johny Thomas <johnythomas.in@gmail.com>
2021-06-05 23:17:03 +0530

    updated covid check

--- stock_checker_temp.py
+++ stock_checker_temp.py
@@ -126,14 +126,14 @@ def covid_center_search():
     if (available_count > 0) and (available_count != history['available_count']):
         push_notification(str(available_count) + " Covid Vaccines Alleppey available for youth")
         if available_count > int(history['available_count']):
-            send_mail("Covid Vaccine available for youth", ",<br/>".join(available_centers)
+            send_mail("Covid Vaccine available for youth Alleppey", ",<br/>".join(available_centers)
                       + "<br/><br/>Total - " + str(available_count)
                       + "<br/><br/> Please subscribe to " + notification_url + " to receive notifications")
 
     if (elder_available_count > 0) and (elder_available_count != history['elder_available_count']):
         push_notification(str(elder_available_count) + " Covid Vaccines Alleppey available for senior citizens")
         if elder_available_count > int(history['elder_available_count']):
-            send_mail("Covid Vaccine available for senior citizens", ",<br/>".join(elder_available_centers)
+            send_mail("Covid Vaccine available for senior citizens Alleppey", ",<br/>".join(elder_available_centers)
                       + "<br/><br/>Total - " + str(elder_available_count)
                       + "<br/><br/> Please subscribe to " + notification_url + " to receive notifications")
     write_file(available_count, elder_available_count, file_path)
